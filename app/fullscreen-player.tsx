4158bb4dc823f2b26c3b57ae6fc6c50b8fbed6a6
xXDLINEXx <solution1nfopro@gmail.com>
2025-11-09 16:52:59 -0500

    Update fullscreen-player.tsx

--- app/fullscreen-player.tsx
+++ app/fullscreen-player.tsx
@@ -1,7 +1,8 @@
-import { useLocalSearchParams } from "expo-router";
-import FullScreenPlayer from "./FullScreenPlayer";
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import FullScreenPlayer from '../components/FullScreenPlayer';
 
-export default function Screen() {
+export default function FullScreenPlayerScreen() {
   const params = useLocalSearchParams<{ mediaId: string }>();
   return <FullScreenPlayer initialMediaId={params.mediaId} />;
 }
